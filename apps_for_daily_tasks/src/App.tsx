d779cc42c4ac8fd42b33efdfec17dd31ed52cd3a
ysko909 <42630040+ysko909@users.noreply.github.com>
2024-04-02 13:13:36 +0900

    Add files via upload

--- apps_for_daily_tasks/src/App.tsx
+++ apps_for_daily_tasks/src/App.tsx
@@ -7,11 +7,56 @@ import Contact from "./routes/Contact";
 export default function App() {
   return (
     <div className="App">
-      <Link to="/">Home</Link>
+      <section>
+        <nav className="navbar" role="navigation" aria-label="main navigation">
+
+          <div id="navbarBasicExample" className="navbar-menu">
+            <div className="navbar-start">
+              <a className="navbar-item" href="Home">
+                Home
+              </a>
+
+              <a className="navbar-item" href="About">
+                About
+              </a>
+
+              <div className="navbar-item has-dropdown is-hoverable">
+                <a className="navbar-link">
+                  More
+                </a>
+
+                <div className="navbar-dropdown">
+                  <a className="navbar-item">
+                    About
+                  </a>
+                  <a className="navbar-item">
+                    Jobs
+                  </a>
+                  <a className="navbar-item" href="Contact">
+                    Contact
+                  </a>
+                  <hr className="navbar-divider" />
+                  <a className="navbar-item">
+                    Report an issue
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false">
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </a>
+          </div>
+        </nav>
+      </section>
+
+      {/* <Link to="/">Home</Link>
       <br />
       <Link to="Contact">Contact</Link>
       <br />
-      <Link to="About">About</Link>
+      <Link to="About">About</Link> */}
 
       <Routes>
         <Route path="/" element={<Home />} />
--- apps_for_daily_tasks/src/index.tsx
+++ apps_for_daily_tasks/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import 'bulma/css/bulma.css'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -9,7 +10,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      
       <App />
+      
     </BrowserRouter>
 
   </React.StrictMode>
